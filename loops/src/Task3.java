4e9a05797cc62941f3a3e19b36d8e5ff5634980e
Arsensarkisian <arsensarkisian229@gmail.com>
2024-12-01 00:08:15 -0600

    Lesson4

--- loops/src/Task3.java
+++ loops/src/Task3.java
@@ -1,7 +1,7 @@
 public class Task3 {
     public static void main(String[] args) {
         int minIndex = 0;
-        int y = 0;
+        int x = 0;
         int array[] = new int[10];
         array = new int[]{1, 6, 7, 3, 23, 56, 44, 33, 23, 73};
         for (int i = 0; i < array.length-1; i++) {
@@ -13,9 +13,9 @@ public class Task3 {
 
                 }
             }
-            y = array[i];
+            x = array[i];
             array[i] = array[minIndex];
-            array[minIndex] = y;
+            array[minIndex] = x;
 
         }
         System.out.println(array[1]);
